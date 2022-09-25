37837a65ced3f276b762f18172bc9531e71350a5
Chiagoziemo <chiagoziemo95@gmail.com>
2022-09-25 22:58:02 +0100

    Auth implementation

--- course-02/exercises/udacity-c2-restapi/www/server.js
+++ course-02/exercises/udacity-c2-restapi/www/server.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // require('dotenv').config();
-require('dotenv').config({ path: '/./../../.env' });
+//require('dotenv').config({path:'/./../../.env'});
 const express_1 = __importDefault(require("express"));
 const sequelize_1 = require("./sequelize");
 const index_router_1 = require("./controllers/v0/index.router");
